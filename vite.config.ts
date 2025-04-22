f84158d94a7d7cc1ddde4839d01d3279a981261b
sparkcanon <prateek_borde@hotmail.com>
2025-04-23 00:03:42 +0100

    Refactor vite.config.ts for improved build configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,24 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from "vite";
+import { resolve } from "path";
 
 export default defineConfig({
   build: {
+    copyPublicDir: false,
     lib: {
-      entry: './lib/main.ts',
-      name: 'Counter',
-      fileName: 'counter',
+      entry: resolve(__dirname, "lib/main.ts"),
+      formats: ["es"],
+      name: "devtoolbar",
+      fileName: "devtoolbar",
+    },
+    rollupOptions: {
+      external: ["react", "react-dom", "react/jsx-runtime"],
+      output: {
+        globals: {
+          react: "React",
+          "react-dom": "React-dom",
+          "react/jsx-runtime": "react/jsx-runtime",
+        },
+      },
     },
   },
-})
+});
